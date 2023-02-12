e86c7e8eb109f7ef01bfb3622ca3b7fb5b39c4b9
Maimoona-Qasmi-3 <maimoonaqasmi3@gmail.com>
2023-02-12 13:20:22 +0500

    Code splitting & GTmetrix

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -20,10 +20,8 @@ const SearchParams = () => {
   const pets = results?.data?.pets ?? [];
 
   return (
-    <div className="my-0 mx-auto w-11/12 ">
-      <form 
-        className="p-10 mb-10 rounded-lg bg-gray-200 shadow-lg flex flex-col
-        justify-center items-center"
+    <div className="search-params">
+      <form
         onSubmit={(e) => {
           e.preventDefault();
           const formData = new FormData(e.target);
@@ -42,13 +40,12 @@ const SearchParams = () => {
         ) : null}
         <label htmlFor="location">
           Location
-          <input className="search-input" type="text" id="location" name="location" placeholder="Location" />
+          <input id="location" name="location" placeholder="Location" />
         </label>
 
         <label htmlFor="animal">
           Animal
           <select
-            className="search-input"
             id="animal"
             name="animal"
             onChange={(e) => {
@@ -69,7 +66,7 @@ const SearchParams = () => {
 
         <label htmlFor="breed">
           Breed
-          <select className="search-input grayed-out-disabled" disabled={!breeds.length} id="breed" name="breed">
+          <select disabled={!breeds.length} id="breed" name="breed">
             <option />
             {breeds.map((breed) => (
               <option key={breed} value={breed}>
@@ -79,7 +76,7 @@ const SearchParams = () => {
           </select>
         </label>
 
-        <button className="rounded px-6 py-2 text-white hover:opacity-50 border-none bg-purple-400">Submit</button>
+        <button>Submit</button>
       </form>
       <Results pets={pets} />
     </div>
